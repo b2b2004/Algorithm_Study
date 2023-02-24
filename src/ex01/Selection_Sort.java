72aaacd23542ec55f24ce76c8ebb8a2acc0b77a5
Kwon yongho <b2b2004@nate.com>
2023-02-24 19:11:07 +0900

    1.sort

---
+++ src/ex01/Selection_Sort.java
@@ -0,0 +1,25 @@
+package ex01;
+
+public class Selection_Sort {
+	// 선택 정렬
+	public static void main(String[] args) {
+		int i, j, min, temp;
+		int index = 0;
+		int[] array = {1, 10, 5, 8, 7, 6, 4, 3, 2, 9};
+		for(i=0;i<10;i++) {
+			min = 9999;
+			for(j=i;j<10;j++) {
+				if(min > array[j]) {
+					min = array[j];
+					index = j;
+				}
+			}
+			temp = array[i];
+			array[i] = array[index];
+			array[index] = temp;
+		}
+		for(i=0;i<10;i++) {
+			System.out.print(array[i] +" ");
+		}
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author KYH
+ *
+ */
+module Algorithm_Study {
+}
